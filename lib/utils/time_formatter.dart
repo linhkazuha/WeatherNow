b60c816184c166331c8afe252039dfb9e1eabaaf
thu03092004@gmail.com <nguyenthihoaithu3904@gmail.com>
2025-06-08 23:23:28 +0700

    update time

--- lib/utils/time_formatter.dart
+++ lib/utils/time_formatter.dart
@@ -3,11 +3,22 @@ class CustomTimeFormatter {
     final now = DateTime.now();
     final difference = now.difference(dateTime);
 
+    // Ánh xạ thứ trong tuần
+    final daysOfWeek = [
+      'Chủ nhật',
+      'Thứ Hai',
+      'Thứ Ba',
+      'Thứ Tư',
+      'Thứ Năm',
+      'Thứ Sáu',
+      'Thứ Bảy'
+    ];
+
     if (difference.isNegative) {
       if (dateTime.day == now.day) {
         return '${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}';
       } else if (dateTime.difference(now).inDays < 7) {
-        return 'T${dateTime.weekday}, ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}';
+        return '${daysOfWeek[dateTime.weekday % 7]}, ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}';
       } else {
         return '${dateTime.day}/${dateTime.month}, ${dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')}';
       }
